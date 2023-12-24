07a5e86e29f5127e8f6364de4a69c41418033ad1
Alkaidcc <alkaidcc@qq.com>
2023-12-24 22:59:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <!-- OK -->
+    <img />
+    <!-- fix on save -->
+    <img>
+    <!-- it will become <img //> -->
+  </div>
+</template>
+
+<style scoped></style>
